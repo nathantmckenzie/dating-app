7a320cf3e1beb91a64a4549bd48c80b15e45c88a
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-03-04 17:09:14 -0800

    Image Uploading Working

---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+import "firebase/storage";
+
+export const app = firebase.initializeApp({
+  projectId: "natetaymac1993",
+  appId: "1:1075308779123:web:84d59d9c3fa6832c99fc06",
+  storageBucket: "natetaymac1993.appspot.com",
+  locationId: "us-central",
+  apiKey: "AIzaSyBqEYZZ3Due0LdYm14dcoN7I0oR3IgPICk",
+  authDomain: "natetaymac1993.firebaseapp.com",
+  messagingSenderId: "1075308779123",
+});
--- src/pages/PhotoUpload.js
+++ src/pages/PhotoUpload.js
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { app } from "../base";
 
-export default function PhotoUpload() {
-  return <div>Photo Upload</div>;
+const db = app.firestore();
+
+function PhotoUpload() {
+  const [fileUrl, setFileUrl] = React.useState(null);
+  const [users, setUsers] = React.useState([]);
+
+  const onFileChange = async (e) => {
+    const file = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl(await fileRef.getDownloadURL());
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    if (!username || !fileUrl) {
+      return;
+    }
+    await db.collection("users").doc(username).set({
+      name: username,
+      avatar: fileUrl,
+    });
+  };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersCollection = await db.collection("users").get();
+      setUsers(
+        usersCollection.docs.map((doc) => {
+          return doc.data();
+        })
+      );
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="file" onChange={onFileChange} />
+        <input type="text" name="username" placeholder="NAME" />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.name}>
+              <img width="300" height="400" src={user.avatar} alt={user.name} />
+              <p>{user.name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
+
+export default PhotoUpload;
